c50c8c3e9a7c48a83993e297b338a70023e51f91
mgstabrani <mgstabrani19@gmail.com>
2020-09-09 10:18:24 +0700

    Creating tandai sudah feature

--- app/Http/Controllers/RandomizerController.php
+++ app/Http/Controllers/RandomizerController.php
@@ -30,4 +30,34 @@ class RandomizerController extends Controller
         // }
         return view('welcome', compact('mhs'));
     }
+
+    public function tandai($id)
+    {
+        $file = fopen("test.csv","r");
+        $current = fgetcsv($file);
+        $mhs = $current;
+        $list = [$current];
+        for ($i = 1; $i<58; $i++)
+        {    
+            $current = fgetcsv($file);      
+            if(intval($current[0]) == $id){
+                $current = [$current[0],$current[1],$current[2],"Sudah"];
+                $mhs = $current;
+            } 
+
+            array_push($list,$current);
+
+        }
+        fclose($file);
+        
+
+        $file = fopen("test.csv","w");
+        foreach($list as $data){
+            fputcsv($file,$data);
+        }
+        fclose($file);
+
+
+        return view('tandai',compact('mhs'));
+    }
 }
